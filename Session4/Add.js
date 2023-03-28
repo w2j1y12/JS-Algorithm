5dc26cb67bbb9e7caac233e7fb227f1ba3f083a3
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-03-28 10:38:51 +0900

    Update Add.js

--- Session4/Add.js
+++ Session4/Add.js
@@ -33,16 +33,25 @@ charCount("Your PIN number is 1234!")
 } */
 
 function charCount(str) {
-    // return an object with keys that are lowercase alphanumeric characters in the string
-    // value should be the counts for those characters
-}
+    // do something
+    // return an object with keys that are lowercase alphanumeric characters in the string;
+    // values should be the counts for those characters
 
-function charCount(str) {
     // make object to return at end
+    var result = {};
     // loop over string, for each character...
+    for(var i = 0; i <str.length; i++) {
+        var char = str[i].toLowerCase()
         // if the char is a number/letter AND is a key in object, add one to count
-        // if the char is a number/letter AND not in object, add it to object set value to 1
-        // if character is something else (space, period, etc.) don't do anything
-   
+        if(result[char] > 0) { // 0보다 크면 이미 객체에 有        
+            result[char] ++;
+        }
+        // if the char is a number/letter AND not in object, add it to object and set value to 1
+        else {
+            result[char] = 1;
+        };
+    }
+        // of charater is someting else (space, period, etc.) don't do anything
     // return object at end
+    return result;
 }
