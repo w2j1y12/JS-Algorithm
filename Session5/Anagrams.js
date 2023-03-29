4e7df792b9c98ed3b5e7063aeee8826512ea27f9
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-03-29 11:26:19 +0900

    Update Anagrams.js

--- Session5/Anagrams.js
+++ Session5/Anagrams.js
@@ -13,3 +13,28 @@ validAnagram('qwerty', 'qeywrt') // true
 validAnagram('texttwisttime', 'timetwisttext') // true
 */
 
+function validAnagram(arr1, arr2){
+  // add whatever parameters you deem necessary - good luck!
+  
+  if(arr1.length !== arr2.length) {
+      return false;
+  }
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {}
+  for(let val of arr1) {
+      frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  }
+  for(let val of arr2) {
+      frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  }
+  for(let key in frequencyCounter1) {
+      if(!(key in frequencyCounter2)) {
+          return false
+      }
+      if(frequencyCounter2[key] !== frequencyCounter1[key]) {
+          return false
+      }
+  }
+  
+  return true
+}
