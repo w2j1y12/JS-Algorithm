6146c48fbbfee294aa69b7ab079c7b274c02659a
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-04-03 16:38:34 +0900

    Create countUniqueValues_Refactor.js

---
+++ Session5/countUniqueValues_Refactor.js
@@ -0,0 +1,11 @@
+function countUniqueValues(arr) {
+    var i = 0;
+    for(var j = 1; j < arr.length; j ++) {
+        if(arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1;
+}
+
