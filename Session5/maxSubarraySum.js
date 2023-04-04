7358a3d73cfae535122a0bbded2e262c1865420d
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-04-04 16:54:01 +0900

    Create maxSubarraySum.js

---
+++ Session5/maxSubarraySum.js
@@ -0,0 +1,31 @@
+/* 
+Write a function called maxSubarraySum which accepts an array of integers and a number called n.
+The function should calculate the maximum sum of n consecutive elements in th array.
+*/
+
+/*
+maxSubarraySum([1,2,5,2,8,1,5],2) // 10
+maxSubarraySum([1,2,5,2,8,1,5],4) // 17
+maxSubarraySum([4,2,1,6],1) // 6
+maxSubarraySum([4,2,1,6,2],4) // 13
+maxSubarraySum([],4) // null
+*/
+
+// Time COmplexity - O(N^2)
+
+function maxSubarraySum(arr, num) {
+    if(num > arr.length) {
+        return null;
+    }
+    var max = -Infinity;
+    for(let i = 0; i < arr.length - num + 1; i ++) {
+        temp = 0;
+        for(let j = 0; j < num; j ++) {
+            temp += arr[i + j];
+        }
+        if(temp > max) {
+            max = temp;
+        }
+    }
+    return max;
+}
