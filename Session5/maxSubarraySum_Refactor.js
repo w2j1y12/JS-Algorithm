8dc5d50b11266262a39360b6f0292232b3c0b18c
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-04-04 16:53:36 +0900

    Create maxSubarraySum_Refactor.js

---
+++ Session5/maxSubarraySum_Refactor.js
@@ -0,0 +1,18 @@
+// Time Complexity - O(N)
+
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+    for(let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for(let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+// 매번 더하는 것이 아니라 처음을 빼고 마지막을 더함
