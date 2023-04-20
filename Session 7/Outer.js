e77f4fee0e3eae952dd84717aff91a795ad38367
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-04-20 09:49:26 +0900

    Create Outer.js

---
+++ Session 7/Outer.js
@@ -0,0 +1,13 @@
+function outer(input) {
+
+    var outerScopedVariable = []
+
+    function helper(helperInput) {
+        // modify the outerScopedVariable
+        helper(helperInput--)
+    }
+
+    helper(input)
+
+    return outerScopedVariable;
+}
