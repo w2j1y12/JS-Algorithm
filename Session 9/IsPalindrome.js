26bc2a40c463476ac44b9115db1b6e2d97419c81
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-04-28 16:17:51 +0900

    Create IsPalindrome.js

---
+++ Session 9/IsPalindrome.js
@@ -0,0 +1,5 @@
+/* 
+Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward).
+Otherwise it returns false.
+*/
+
