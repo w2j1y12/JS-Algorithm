c1f029701f057024a9bc9a3c5117754a828f145b
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-04-28 15:43:29 +0900

    Update Reverse.js

--- Session 9/Reverse.js
+++ Session 9/Reverse.js
@@ -1,3 +1,12 @@
 /*
 Write a recursive function called reverse which accepts a string and returns a new string in reverse
 */
+
+function reverse(str){
+  // add whatever parameters you deem necessary - good luck!
+  if(str.length <= 1) return str
+  else return reverse(str.slice(1)) + str[0]
+}
+
+reverse('awesome')
+reverse('rithmschool')
