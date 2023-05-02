864e4d8aaf6fd8056f97a03592ec8949f1bdc5be
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-05-02 16:31:23 +0900

    Update IsPalindrome.js

--- Session 9/IsPalindrome.js
+++ Session 9/IsPalindrome.js
@@ -3,3 +3,39 @@ Write a recursive function called isPalindrome which returns true if the string 
 Otherwise it returns false.
 */
 
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(str){
+  // add whatever parameters you deem necessary - good luck!
+  newStr = ''
+  
+  if(str.length <= 1) return newStr
+  
+  newStr = isPalindrome(str.slice(1)) + str[0] 
+  return newStr
+  
+  console.log(newStr)
+  
+  if(newStr === str) return true
+  else return false
+}
+
+isPalindrome('awesome')
+isPalindrome('foobar')
+isPalindrome('tacocat')
+isPalindrome('amanaplanacanalpanama')
+isPalindrome('amanaplanacanalpandemonium')
+
+// 나는 뒤집은 걸 newStr에 넣어서 기존 str이랑 비교하고 싶었는데 실패
+
+/* 
+Expected 'mosewa' to equal false.
+Expected 'aboof' to equal false.
+Expected 'acocat' to equal true.
+Expected 'manaplanacanalpanama' to equal true.
+Expected 'uinomednaplanacanalpanama' to equal false.
+*/
