600df18a40bac9b85b7fde3fd29bee063e389d74
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-05-02 16:37:07 +0900

    Update IsPalindrome_Solution.js

--- Session 9/IsPalindrome_Solution.js
+++ Session 9/IsPalindrome_Solution.js
@@ -1,6 +1,16 @@
 function isPalindrome(str){
-    if(str.length === 1) return true;
-    if(str.length === 2) return str[0] === str[1];
-    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
-    return false;
+  // add whatever parameters you deem necessary - good luck!
+  if(str.length === 1) return true;
+  // str 문자열이 홀수일 경우
+  if(str.length === 2) return str[0] === str[1];
+  // str 문자열이 짝수일 경우
+  if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+  return false;
+  
 }
+
+isPalindrome('awesome')
+isPalindrome('foobar')
+isPalindrome('tacocat')
+isPalindrome('amanaplanacanalpanama')
+isPalindrome('amanaplanacanalpandemonium')
