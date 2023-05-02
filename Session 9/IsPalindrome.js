c44c37cd8661595a2a9c29b4089f040244adb9fc
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-05-02 16:37:20 +0900

    Update IsPalindrome.js

--- Session 9/IsPalindrome.js
+++ Session 9/IsPalindrome.js
@@ -3,12 +3,6 @@ Write a recursive function called isPalindrome which returns true if the string 
 Otherwise it returns false.
 */
 
-// isPalindrome('awesome') // false
-// isPalindrome('foobar') // false
-// isPalindrome('tacocat') // true
-// isPalindrome('amanaplanacanalpanama') // true
-// isPalindrome('amanaplanacanalpandemonium') // false
-
 function isPalindrome(str){
   // add whatever parameters you deem necessary - good luck!
   newStr = ''
