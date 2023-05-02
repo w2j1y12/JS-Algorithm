85b64f221189eeb342af6f39b9cf40782ff2122c
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-05-02 20:01:30 +0900

    Create SomeRecursive.js

---
+++ Session 9/SomeRecursive.js
@@ -0,0 +1,6 @@
+/*
+Write a recursive function called someRecursive which accpets an array and a callback.
+The function returns true if a single value in the array returns true when passed to the callback.
+Other it returns false.
+*/
+
