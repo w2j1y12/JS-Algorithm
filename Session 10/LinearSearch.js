4a0817df629e474785517c7a549121eb921ad90a
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-05-16 15:29:14 +0900

    Update LinearSearch.js

--- Session 10/LinearSearch.js
+++ Session 10/LinearSearch.js
@@ -16,10 +16,10 @@ Linear Search Pseudocode
 function linearSearch(arr, value){
   // add whatever parameters you deem necessary - good luck!
   
-  for(let i=0; i<arr.length; i++) {
-      if(arr[i] === value) i
-      else return -1
+  for(var i=0; i<arr.length; i++) {
+      if(arr[i] === value) return i;
   }
+  return -1
 }
 
 linearSearch([10, 15, 20, 25, 30], 15)
