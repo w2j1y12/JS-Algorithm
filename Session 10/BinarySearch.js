800b5ccdcc5c37ab9783bd43f0b246086bcd1e3b
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-05-18 11:35:34 +0900

    Update BinarySearch.js

--- Session 10/BinarySearch.js
+++ Session 10/BinarySearch.js
@@ -1,4 +1,13 @@
 /*
+Write a function called binarySearch which accpets a sorted array and a value and return the index at which the value exists.
+Otherwise, return -1.
+
+This algorithm should be more efficient than linearSearch
+- you can read how to implement it here
+https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search and here - https://www.topcoder.com/community/data-science/data-science-tutorials/binary-search/
+*/
+
+/*
 Binary Search Pseudocode
 
 - This function accpets a sorterd array and a value
